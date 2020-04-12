ad4b5bb05ade00a470b89582887be9f67fc228cf
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-04-12 18:41:39 +0700

    Tugas 5 PHP

---
+++ tugas5_1.php
@@ -0,0 +1,17 @@
+<?php
+echo "Tabel Logika 1111";
+echo "<table border='1'>";
+echo "<tr>";
+echo "<td>Input true</td>";
+echo "<td>Input 2</td>";
+echo "<td>AND</td>";
+echo "<td>OR</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Input true</td>";
+echo "<td>Input 2</td>";
+echo "<td>AND</td>";
+echo "<td>OR</td>";
+echo "</tr>";
+?>
